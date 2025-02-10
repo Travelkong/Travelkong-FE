a3496f8dbc08f2a8785edacc9a3bd216e068376f
thuan122 <127835141+thuan122@users.noreply.github.com>
2025-02-10 19:02:29 +0700

    Feat: browser router

--- src/Routes/App.tsx
+++ src/Routes/App.tsx
@@ -1,7 +1,52 @@
 import "@/Styles/index.css"
+import routes from "./Path"
+import { DefaultLayout } from "@/Layouts"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import React, { Fragment } from "react"
+
+interface DefaultLayoutProps {
+  children: React.ReactNode
+}
+
+interface RouteProps {
+  path: string
+  component: React.ComponentType
+  layout?: React.ComponentType<DefaultLayoutProps> | null
+}
 
 function App() {
-  return <p>Do something</p>
+  return (
+    <Router>
+      <Routes>
+        {routes.map((route: RouteProps, index: number) => {
+          let Layout
+
+          if (route.layout) {
+            Layout = route.layout
+          } else if (route.layout === null) {
+            Layout = Fragment
+          } else {
+            Layout = DefaultLayout
+          }
+
+          const Element = route.component
+
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout>
+                  <Element />
+                </Layout>
+              }
+            />
+          )
+        })}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
